3cc5e0847ff15c45222196016aed2f731096b829
sumit-panigrahi <sumit-panigrahi@outlook.com>
2025-02-19 00:55:44 +0530

    remove static files

--- storybook-static/assets/preview-CeC4kQej.js
+++
@@ -1,1 +0,0 @@
-import{N as o}from"./index-DKqesacJ.js";import"./index-DmM0KDA7.js";const t={parameters:{controls:{matchers:{color:/(background|color)$/i,date:/Date$/i}},docs:{theme:o.dark}}};export{t as default};
